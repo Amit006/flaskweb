2e0e392db0c2dfd931dc7a82711c0b3ac36d7845
Amit Nayek <anayek@newamps.co.in>
2020-04-14 17:15:56 +0530

    Successfully Signup

--- APP.py
+++ APP.py
@@ -1,12 +1,13 @@
 
 from flask import Flask, render_template, request, flash, redirect, url_for, session
 from flask_mysqldb import MySQL
+# from sqlalchemy.engine import default
 from werkzeug.datastructures import MultiDict
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 from wtforms.fields.html5 import EmailField
 
 from flask_bootstrap import Bootstrap
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 
 
@@ -22,22 +23,21 @@ bootstrap = Bootstrap(app)
 
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'N#123456'
-app.config['MYSQL_DB'] = 'FalskDb'
+app.config['MYSQL_DB'] = 'FlaskDB'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
 
   # MODEL PORTION
-class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
+class SignUpFrom(Form):
+    username = StringField('username', [validators.Length(min=4, max=25)])
+    email = StringField('email', [validators.Length(min=6, max=35)])
+    password = PasswordField('password', [
         validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
+        validators.EqualTo('repassword', message='Passwords must match')
     ])
-    confirm = PasswordField('Repeat Password')
-    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+    repassword = PasswordField('repassword')
 
 class SignInForm(Form):
     email = EmailField('email', [validators.DataRequired(), validators.Email()])
@@ -45,13 +45,46 @@ class SignInForm(Form):
     rememberMe = BooleanField('rememberMe')
 
 
-
 @app.route('/')
 def index():
+    form = SignUpFrom(request.form)
+
     return render_template('index.html', name='Amit Nayek')
 
-@app.route('/Signup')
+@app.route('/Signup', methods=['GET', 'POST'])
 def SignUp():
+    form = SignUpFrom(request.form)
+
+    if request.method == 'POST':
+        username = request.form['username'] if request.form['username'] else ''
+        email = request.form['email'] if request.form['email'] else ''
+        password = request.form['password'] if request.form['password'] else ''
+        confirm = request.form['repassword'] if request.form['repassword'] else ''
+        print(' username: ', username, ' password: ', password, ' email: ', email, ' confirm: ', confirm)
+
+        print(' validation status: ', form.validate())
+        print(' validation errors: ', form.errors)
+
+        if form.validate():
+
+            flash(u'Successfully SignUP', 'Success')
+            # con = mysql.connect()
+            # cur = con.cursor()
+            cur = mysql.connection.cursor()
+            cur.execute('''INSERT INTO 
+                  users (
+                      User_id,
+                      Uname,
+                      Uemail,
+                      Upassword
+                      )
+              VALUES (default ,%s,%s,%s)''', ( username, email, password))
+            mysql.connection.commit()
+            # return redirect(url_for('Dashboard'))
+
+        else:
+            flash(u'All the form fields are required. ', 'Error')
+
     return render_template('Signup.html',)
 
 @app.route('/SignIn', methods=['GET', 'POST'])
@@ -78,11 +111,17 @@ def SignIn():
             # db_session.add(user)
 
             flash(u'Successfully SignIn', 'Success')
+            cur = mysql.connection.cursor()
+            cur.execute(''' select * from  users where Uemail = %s''',(email))
+            results = cur.fetchall()
+            print(' ****************** Results ***************')
+            print(results)
+
             # return redirect(url_for('Dashboard'))
         else:
             flash(u'All the form fields are required. ', 'Error')
 
-    return render_template('Signin.html', form=form)
+    return render_template('Signin.html', form=form, results=results)
 
 
 
